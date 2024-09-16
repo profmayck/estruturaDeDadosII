a49c17d233facaa920ee4d82cd63140fd95770f6
profmayck <85135029+profmayck@users.noreply.github.com>
2024-09-16 01:58:59 -0300

    Funções de impressão

--- arvoresBinarias/main.c
+++ arvoresBinarias/main.c
@@ -194,3 +194,109 @@ No* buscarMenorValor(No* raiz){
     // Do contrário, retornar o próprio nó (raiz)
     return raiz;
 }
+void imprimirEmOrdem(No* raiz){
+	if( raiz == NULL ){
+		return;
+	}
+	imprimirEmOrdem(raiz->esquerda);
+	printf("%d\n", raiz->valor);
+	imprimirEmOrdem(raiz->direita);
+	return;
+}
+
+void imprimirPreOrdem(No* raiz){
+	if( raiz == NULL ){
+		return;
+	}
+	printf("%d\n", raiz->valor);
+	imprimirPreOrdem(raiz->esquerda);
+	imprimirPreOrdem(raiz->direita);
+	return;
+}
+
+void imprimirPosOrdem(No* raiz){
+	if( raiz == NULL ){
+		return;
+	}
+	imprimirPosOrdem(raiz->esquerda);
+	imprimirPosOrdem(raiz->direita);
+	printf("%d\n", raiz->valor);
+	return;
+}
+
+void imprimirArvoreEmProfundidade(No* raiz){
+    /*
+    (Imprimindo árvores) Escreva uma função recursiva saidaArvore para exibir
+    uma árvore binaria na tela. A função deve exibir a árvore, linha após linha,
+    com o topo da árvore na esquerda da tela e o final da árvore no lado direito
+    da tela. Cada linha é exibida verticalmente. Por exemplo, a árvore binaria
+    ilustrada fixa nesse exercício é exibida da seguinte maneira:
+
+                    42
+               40
+                    35
+          19
+                    18
+               17
+                    16
+     15
+                    14
+               13
+                    12
+          11
+                     9
+                8
+                     7
+
+    Observe que o nó da extremidade direita aparece no topo da saída, na coluna
+    da extremidade direita, e que o nó raiz aparece no lado esquerdo da saída.
+    Cada coluna da saída inicia cinco espaços à direita da coluna anterior. A
+    função saidaArvore deve receber como argumentos um ponteiro para o nó raiz da
+    árvore e um inteiro totalEspacos representando o número de espaços que
+    antecedem o valor a ser exibido (essa variável deve iniciar com zero para que
+    a exibição do nó raiz seja no lado esquerdo da tela). A função usa um
+    percurso inorder modificado para exibir a árvore — ele inicia com o nó da
+    extremidade direita da tela e segue em direção à esquerda. O algoritmo é o
+    seguinte:
+        Enquanto o ponteiro para o nó atual não for NULL
+            Chame recursivamente saidaArvore com a subárvore direita do nó atual
+            e totalEspacos + 5
+            Use uma estrutura for para contar de 1 a totalEspacos e exiba os
+            espaços
+            Exiba o valor no nó atual
+            Defina o ponteiro para o nó atual para apontar para a subárvore
+            esquerda do nó atual Incremente totalEspacos de 5.
+*/
+    if(raiz == NULL){
+        return;
+    }
+    
+    imprimirArvoreEmProfundidade(raiz->direita);
+    printf("%*s%*d\n",raiz->profundidade * 5, "", 3, raiz->valor);
+    imprimirArvoreEmProfundidade(raiz->esquerda);
+    return;
+}
+void imprimirArvore(No* raiz){
+    return;
+}
+void imprimirSubArvore(No* raiz, int valor){
+    printf("\n\nEm Ordem do nó %d:\n", valor);
+    printf("----------------------------\n");
+    imprimirEmOrdem(buscarNo(raiz, valor));
+}
+void imprimirPercorrer(No* raiz){
+    // percorre a árvore em ordem
+    printf("\n\nEm Ordem:\n");
+    printf("----------------------------\n");
+    imprimirEmOrdem(raiz);
+    
+    // percorre a árvore em pré ordem
+    printf("\n\nPre Ordem:\n");
+    printf("----------------------------\n");
+    imprimirPreOrdem(raiz);
+    
+    // percorre a árvore em pré ordem
+    printf("\n\nPos Ordem:\n");
+    printf("----------------------------\n");
+    imprimirPosOrdem(raiz);
+}
